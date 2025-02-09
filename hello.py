6182c90bca41e3a0ccbea0793f79978036774b8c
ANOS VOLDIGOAD <omparida90400@gmail.com>
2025-02-09 11:54:41 +0530

    First program

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("hello");
+print("hello" );
+print("hello" );
+print("");
+
+# Adding two numbers
+num1 = 5
+num2 = 3
+result = num1 + num2
+print("The sum of", num1, "and", num2, "is", result)
